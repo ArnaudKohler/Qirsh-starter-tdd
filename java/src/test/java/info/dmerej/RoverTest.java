cd83e55e181cc0f403d400cf74dbccdb2871dd62
Rebecca <rebecca.morel@efrei.net>
2025-01-15 11:22:24 +0100

    update

--- java/src/test/java/info/dmerej/RoverTest.java
+++ java/src/test/java/info/dmerej/RoverTest.java
@@ -1,28 +1,23 @@
 package info.dmerej;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.CsvFileSource;
+ 
 
 public class RoverTest {  
 
     @ParameterizedTest
-    @CsvSource({
-        "0, 0, north, 0, 1",
-        "0, 0, south, 0,-1",
-        "0, 0, east,  1, 0",
-        "0, 0, west, -1, 0"
-    })
-    void movingForwardTest(int x, int y, String direction, int expectedX, int expectedY) {
-        // Given a rover at position (x, y)
+    @CsvFileSource(resources="./csv/movingForward.csv", numLinesToSkip=1 ) 
+    void movingForwardTest(int x,int y, String direction,int expectedX, int expectedY){
         Rover rover = new Rover(x, y, direction);
 
         // When the rover moves forward
         rover.moveForward(rover.position.direction);
 
-        // Then the rover should be at the expected position
+        // Then the rover should be at position (0, 1) facing North
         assertEquals(expectedX, rover.getX());
         assertEquals(expectedY, rover.getY());
     }
+
 }
